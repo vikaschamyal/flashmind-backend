afe8f2ede8b54f0658528e23bc6bf601f29362bb
vikaschamyal <vikaschmayal@gmail.com>
2025-08-08 19:53:47 +0530

    update mongoose schema add username

--- models/User.js
+++ models/User.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+  },
   email: {
     type: String,
     required: true,
@@ -11,5 +15,6 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
+
 
 module.exports = mongoose.model("User", userSchema);
