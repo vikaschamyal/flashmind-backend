d22c839fb0adbd5994f96752c54850f5a03b63ac
vikaschamyal <vikaschmayal@gmail.com>
2025-08-08 20:20:25 +0530

    Add a pre-save hook to auto-generate the username from email

--- models/User.js
+++ models/User.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  
   email: {
     type: String,
     required: true,
@@ -11,7 +10,18 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  username: {
+    type: String,
+    unique: true,
+  },
 });
 
+// Add a pre-save hook to auto-generate the username from email
+userSchema.pre("save", function (next) {
+  if (!this.username && this.email) {
+    this.username = this.email.split("@")[0]; // generate from email
+  }
+  next();
+});
 
 module.exports = mongoose.model("User", userSchema);
