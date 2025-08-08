4853a45e5d7c30881919e87a6fa15f8fc5dfdd01
vikaschamyal <vikaschmayal@gmail.com>
2025-08-08 20:01:59 +0530

    username reamoved from schema

--- models/User.js
+++ models/User.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    unique: true,
-  },
+  
   email: {
     type: String,
     required: true,
